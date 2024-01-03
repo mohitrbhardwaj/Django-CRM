7d2b7ff1bf7f1fac70b279b5e742f4ff071d7cd4
Mohit Sharma <mohitrbhardwaj@gmail.com>
2024-01-04 00:09:42 +0530

    Deletion of db records

--- website/urls.py
+++ website/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('logout/', views.logout_user, name = 'logout'),
     path('signup/', views.signup_user, name = 'signup'),
     path('record/<int:pk>', views.user_record, name = 'record'),
+    path('delete_record/<int:pk>', views.delete_record, name = 'delete_record'),
 ]
--- website/views.py
+++ website/views.py
@@ -56,3 +56,13 @@ def user_record(request, pk):
     else:
         messages.success(request, "You must be logged in to access user records")
         return redirect('home')
+    
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Record deleted successfully!")
+        return redirect('home')
+    else:
+        messages.success(request, "You must be logged in to access user records")
+        return redirect('home')
